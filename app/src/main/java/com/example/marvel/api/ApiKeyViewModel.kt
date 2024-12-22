9d12bafbdb3902b8be28301d564e56631420006f
JHouston <jamesmolochko@gmail.com>
2024-12-23 01:48:19 +0600

    lab2

---
+++ app/src/main/java/com/example/marvel/api/ApiKeyViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.marvel.api
+
+import androidx.lifecycle.ViewModel
+
+class ApiKeyViewModel : ViewModel() {
+    val apiKey = "b9df47947ab951d8c0a86014efe43711"
+    val privateKey = "9c213ff9bd717d9da92e0ce3798ce9903baf19ba"
+}
--- app/src/main/java/com/example/marvel/data/AppUrls.kt
+++ app/src/main/java/com/example/marvel/data/AppUrls.kt
@@ -1,31 +1,5 @@
 package com.example.marvel.data
-import com.example.marvel.R
-
-
-data class HeroData(
-    val imageUrl: String,
-    val nameResId: Int,
-    val descriptionResId: Int
-)
 
 object AppUrls {
     const val MARVEL_LOGO_URL = "https://iili.io/JMnuvbp.png"
-
-    val heroes = listOf(
-        HeroData(
-            imageUrl = "https://iili.io/JMnuyB9.png",
-            nameResId = R.string.hero_name_spider_man,
-            descriptionResId = R.string.hero_description_spider_man
-        ),
-        HeroData(
-            imageUrl = "https://iili.io/JMnuDI2.png",
-            nameResId = R.string.hero_name_iron_man,
-            descriptionResId = R.string.hero_description_iron_man
-        ),
-        HeroData(
-            imageUrl = "https://iili.io/JMnAfIV.png",
-            nameResId = R.string.hero_name_deadpool,
-            descriptionResId = R.string.hero_description_deadpool
-        )
-    )
 }
